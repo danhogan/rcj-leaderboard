41139a016cd621ff3d3a5f90cf5888bfa545abc6
Dan Hogan <danhogan78@gmail.com>
2021-10-20 10:35:19 -0400

    changing colors for rcj

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 import colors from "vuetify/lib/util/colors";
+//https://vuetifyjs.com/en/styles/colors/#material-colors
 
 Vue.use(Vuetify);
 
@@ -10,14 +11,14 @@ export default new Vuetify({
         dark: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
         themes: {
             light: {
-                primary: "#336799",
-                secondary: "#f04a24",
+                primary: colors.orange.darken2,
+                secondary: colors.blue,
                 accent: colors.shades.black,
                 // error: colors.red.accent3,
             },
             dark: {
-                primary: "#336799",
-                secondary: "#f04a24",
+                primary: colors.orange.darken2,
+                secondary: colors.blue,
             },
         },
     },
